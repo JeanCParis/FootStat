65e646ce6084fd60c1f58aaaf8631ddd475c1f98
Jean-Christophe Pâris <jeancparis@hotmail.com>
2014-07-03 23:41:14 +0200

    Work in progress : Championnats table completed, started with the implementation of the Equipes table and the associated functions

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,22 +1,24 @@
 function dbError(err) {
-    alert("Error processing SQL : " + err);
+    alert("Error processing SQL : " + err.code);
 }
 
 function onDeviceReady() {
     database = window.openDatabase("Database", "1.0", "FootStat", 200000);
     database.transaction(
             function(tx) {
-                tx.executeSql('CREATE TABLE IF NOT EXISTS Championnat (id INTEGER PRIMARY KEY, nom TEXT)');
-                
+                tx.executeSql('DROP TABLE IF EXISTS Championnats');
+                tx.executeSql('CREATE TABLE IF NOT EXISTS Championnats (id INTEGER PRIMARY KEY AUTOINCREMENT, nom VARCHAR(30) NOT NULL, pays VARCHAR(30) NOT NULL, nombre_equipes TINYINT NOT NULL)');
+                tx.executeSql('DROP TABLE IF EXISTS Equipes');
+                tx.executeSql('CREATE TABLE IF NOT EXISTS Equipes (id INTEGER PRIMARY KEY AUTOINCREMENT, nom VARCHAR(30) NOT NULL, abreviation VARCHAR(3) NOT NULL)');
             },
             dbError
             );
     
-    $.get('js/templates.html', function(templates) {
+    $.ready($.get('js/templates.html', function(templates) {
         var header = $(templates).filter('#header').html();
         var page = $(templates).filter('#main-menu').html(); 
         $("#container").html(header).append(page);
-    }, 'html');
+    }, 'html'));
 }
 
 function loadMain() {
@@ -40,11 +42,13 @@ function loadConsulterChampionnats() {
                 var championnats = "";
                 var back_button = "</br><button onclick='loadMain()'>Back</button>";              
                 if(result.rows.length == 0) {
-                    championnats = "<div>Pas de championnats enregistre</div></br>";
+                    championnats = "<div>Pas de championnats enregistrés</div></br>";
                 }
                 else {
-                    for(var i=0 ; i<result.rows.length ; ++i) {
-                        championnats += "<div onclick='consulterChampionnat(" + result.rows.item(i).id + ")'>" + result.rows.item(i).nom + "</div>";
+                    for(var i=0 ; i<result.rows.length ; ++i) {   
+                        var id = result.rows.item(i).id;
+                        var nom = result.rows.item(i).nom;
+                        championnats += "<div onclick='consulterChampionnat("+ id +",\""+ nom +"\")'>" + nom + "</div>";
                         championnats += "</br>";
                     }
                 }
@@ -58,22 +62,26 @@ function loadConsulterChampionnats() {
     }, 'html');
 }
 
-function consulterChampionnat(id){
+function consulterChampionnat(id, nom){
     $.get('js/templates.html',
     function(templates) {
         var header = $(templates).filter('#header').html();
         var page = $(templates).filter('#consulter_championnats').html();
-        var effacer = "</br><button onclick=effacerChampionnat(" + id + ")>Effacer</button>";
+        var title = "<h2>" + nom + "</h2>";
+        var effacer = "<button onclick=effacerChampionnat("+id+",\""+nom+"\")>Effacer</button>";
         var back_button = "</br></br><button onclick='loadConsulterChampionnats()'>Back</button>";
-        $("#container").html(header).append(page).append(effacer).append(back_button);
+        $("#container").html(header).append(page).append(title).append(effacer).append(back_button);
     }, 'html');
 }
 
-function effacerChampionnat(id){
+function effacerChampionnat(id, nom){
     database.transaction(
         function(tx) {
-            tx.executeSql('DELETE FROM Championnat WHERE id=?',[id],
-            function(){}, dbError
+            tx.executeSql('DELETE FROM Championnats WHERE id=?',[id],
+            function(){
+                alert("Championnat "+ nom +" effacé !");
+            },
+            dbError
             );   
         },
         dbError
@@ -93,41 +101,122 @@ function loadAjouterChampionnat() {
 function enregistrerAjoutChampionnat() {    
     resetFormulaireChampionnat();
     
-    var nom = $('#nom')
+    var nom = $('#nom');
     var nbr = $('#nbr_equipes');
     var pays = $('#pays');
     var ok = true;
+    var test = "lala";
     
-    if(nom.val().length > 16) {
-        nom.after("<div id='error'>Trop long !</div>");
+    if (nom.val().length == 0) {
+        nom.after("<div id='error'>Nom de championnat manquant !</div>");
+        ok = false;   
+    } else if(nom.val().length > 16) {
+        nom.after("<div id='error'>Le nom d'un championnat est limité a 16 caractères</div>");
         ok = false;
     }
     
-    if(!$.isNumeric(nbr.val())) {
-        nbr.after("<div id='error'>Ceci n'est pas un nombre</div>");
+    if(nbr.val() == 0) {
+        nbr.after("<div id='error'>Nombre d'équipes manquant !</div>");
         ok = false;
     }
+
     if (ok) {
         database.transaction(
             function(tx) {
-                tx.executeSql('SELECT * FROM Championnat WHERE nom=?',[nom.val()],
+                tx.executeSql('SELECT * FROM Championnats WHERE nom=?',[nom.val()],
                 function(tx, result) {
                     if(result.rows.length != 0) {
-                        ok = false;
-                        nom.after("<div id='error'>Un championnat du meme nom existe deja !</div>");
+                        nom.after("<div id='error'>Un championnat du même nom existe déjà !</div>");
                     }
                     else {
-                        database.transaction(
-                            function(tx) {
-                            tx.executeSql('INSERT INTO Championnat (nom) VALUES (?)', [nom.val()]);
-                            }, dbError);
+                        tx.executeSql('INSERT INTO Championnats (nom, pays, nombre_equipes) VALUES (?,?,?)', [nom.val(), pays.val(), parseInt(nbr.val())]);
+                        loadConsulterChampionnats();
                     }
                 }, dbError);
-            });
+            },
+            dbError);
     }
 }
 
 function resetFormulaireChampionnat() { 
+    $("#error").each( function(index, element) {
+        $(this).remove() });
+}
+
+function loadConsulterEquipes() {
+    $.get('js/templates.html', function(templates) {
+    var header = $(templates).filter('#header').html();
+    var page = $(templates).filter('#consulter_equipes').html();
+    $("#container").html(header).append(page);
+    
+    database.transaction(
+        function(tx) {
+            tx.executeSql('SELECT * FROM Equipes',[],
+            function(tx, result) {
+                var equipes = "";
+                var back_button = "</br><button onclick='loadMain()'>Back</button>";              
+                if(result.rows.length == 0) {
+                    equipes = "<div>Pas d'équipes enregistrées</div></br>";
+                }
+                else {
+                    for(var i=0 ; i<result.rows.length ; ++i) {   
+                        var id = result.rows.item(i).id;
+                        var nom = result.rows.item(i).nom;
+                        equipes += "<div onclick='consulterEquipe("+ id +",\""+ nom +"\")'>" + nom + "</div>";
+                        equipes += "</br>";
+                    }
+                }
+                $("#container").append(equipes).append(back_button);
+            },
+            dbError
+            );   
+        },
+        dbError
+    );
+    }, 'html');
+}
+
+function consulterEquipe(id, nom){
+    $.get('js/templates.html',
+    function(templates) {
+        var header = $(templates).filter('#header').html();
+        var page = $(templates).filter('#consulter_equipes').html();
+        var title = "<h2>" + nom + "</h2>";
+        var effacer = "<button onclick=effacerEquipe("+id+",\""+nom+"\")>Effacer</button>";
+        var back_button = "</br></br><button onclick='loadConsulterEquipes()'>Back</button>";
+        $("#container").html(header).append(page).append(title).append(effacer).append(back_button);
+    }, 'html');
+}
+
+function effacerEquipe(id, nom){
+    database.transaction(
+        function(tx) {
+            tx.executeSql('DELETE FROM Equipes WHERE id=?',[id],
+            function(){
+                alert("Equipe "+ nom +" effacée !");
+            },
+            dbError
+            );   
+        },
+        dbError
+    );
+    loadConsulterEquipes();
+}
+
+function loadAjouterEquipe() {
+    $.get('js/templates.html', function(templates) {
+    var header = $(templates).filter('#header').html();
+    var page = $(templates).filter('#ajouter_equipe').html();
+    var back_button = "</br><button onclick='loadMain()'>Back</button>"
+    $("#container").html(header).append(page).append(back_button);
+}, 'html');
+}
+
+function enregistrerAjoutEquipe() {    
+    resetFormulaireEquipe();
+}
+
+function resetFormulaireEquipe() { 
     $("#error").each( function(index, element) {
         $(this).remove() });
 }
